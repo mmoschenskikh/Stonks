60723f75c5154bb5e0583d88b5028ec90a5fac0a
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-08 16:05:40 +0300

    Gradle files migrated to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinKapt)
+    id(BuildPlugins.ktLint)
+}
+
+android {
+    compileSdkVersion(AppConfig.Sdk.compile)
+
+    defaultConfig {
+        applicationId = AppConfig.appId
+        minSdkVersion(AppConfig.Sdk.min)
+        targetSdkVersion(AppConfig.Sdk.target)
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner = AppConfig.testRunner
+    }
+
+    buildFeatures.viewBinding = true
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Dependencies.kotlin)
+    implementation(Dependencies.materialDesign)
+
+    implementation(Android.appcompat)
+    implementation(Android.constraintLayout)
+    implementation(Android.coreKtx)
+
+    testImplementation(Testing.jUnit)
+    androidTestImplementation(Testing.extJUnit)
+    androidTestImplementation(Testing.espresso)
+}
+
+ktlint {
+    android.set(true)
+    outputColorName.set("RED")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath(BuildPlugins.gradle)
+        classpath(BuildPlugins.kotlinGradle)
+        classpath(Dependencies.ktLint)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
