84e64fee06c757e141d24d132bc8e972c78a212a
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-10 18:29:06 +0300

    API key store added

---
+++ app/src/main/java/ru/maxultra/stonks/ApiKeyStorage.kt
@@ -0,0 +1,7 @@
+package ru.maxultra.stonks
+
+/**
+ * Stores Financial Modeling Prep API key.
+ */
+// TODO: Replace with your own API key (https://financialmodelingprep.com/register)
+internal const val FMP_API_KEY = "YOUR_API_KEY_HERE"
