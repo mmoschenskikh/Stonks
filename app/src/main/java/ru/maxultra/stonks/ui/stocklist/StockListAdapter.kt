9f0ddc1bef6b4fdaee3d571a435f751a2140b27a
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-16 22:09:31 +0300

    Implemented Adapter and ViewHolder

--- app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockListAdapter.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockListAdapter.kt
@@ -1,29 +1,33 @@
 package ru.maxultra.stonks.ui.stocklist
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import ru.maxultra.stonks.data.model.Stock
+import ru.maxultra.stonks.databinding.ItemStockBinding
 
 class StockListAdapter : ListAdapter<Stock, StockViewHolder>(DIFF_CALLBACK) {
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Stock>() {
-            override fun areItemsTheSame(oldItem: Stock, newItem: Stock): Boolean {
-                TODO("Not yet implemented")
-            }
+            override fun areItemsTheSame(oldItem: Stock, newItem: Stock) =
+                oldItem.ticker == newItem.ticker
 
-            override fun areContentsTheSame(oldItem: Stock, newItem: Stock): Boolean {
-                TODO("Not yet implemented")
-            }
+            override fun areContentsTheSame(oldItem: Stock, newItem: Stock) =
+                oldItem == newItem
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        StockViewHolder(
+            ItemStockBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
-    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) =
+        holder.bind(getItem(position))
 }
--- app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockViewHolder.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockViewHolder.kt
@@ -1,12 +1,14 @@
 package ru.maxultra.stonks.ui.stocklist
 
 import androidx.recyclerview.widget.RecyclerView
+import ru.maxultra.stonks.data.model.Stock
 import ru.maxultra.stonks.databinding.ItemStockBinding
 
 class StockViewHolder(private val binding: ItemStockBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun bind() {
-        TODO()
+    fun bind(stock: Stock) {
+        binding.tickerText.text = stock.ticker
+        binding.nameText.text = stock.companyName
     }
 }
