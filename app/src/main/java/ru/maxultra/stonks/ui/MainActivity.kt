db029d71a7abe469c58556fa5e2eae2007928da9
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-17 13:37:30 +0300

    Set up search bar instead of action bar

--- app/src/main/java/ru/maxultra/stonks/ui/MainActivity.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package ru.maxultra.stonks.ui
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.maxultra.stonks.R
+import ru.maxultra.stonks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = null
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.statusBarColor = Color.BLACK
+        }
     }
 }
