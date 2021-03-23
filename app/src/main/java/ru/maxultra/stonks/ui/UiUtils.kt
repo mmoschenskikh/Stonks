8857d445379e1990afecd71d4b8640e90be1d7a5
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-24 00:13:48 +0300

    Utils for price displaying

--- app/src/main/java/ru/maxultra/stonks/ui/UiUtils.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/UiUtils.kt
@@ -9,6 +9,10 @@ import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat
 import ru.maxultra.stonks.R
 import ru.maxultra.stonks.databinding.SearchBarBinding
+import ru.maxultra.stonks.round
+import java.text.NumberFormat
+import java.util.*
+import kotlin.math.abs
 
 /**
  * Provides desirable behaviour of search bar.
@@ -49,4 +53,26 @@ fun SearchBarBinding.manageSearchBar(context: Context) {
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun formatPrice(currency: Currency, price: Double): String =
+    // Cannot use single instance of NumberFormat because of possible configuration change
+    NumberFormat.getCurrencyInstance().also { it.currency = currency }.format(price)
+
+fun formatPriceDelta(currency: Currency, price: Double, priceDelta: Double): String {
+    val sign = when {
+        priceDelta > 0 -> "+"
+        priceDelta < 0 -> "-"
+        else -> ""
+    }
+    val absDelta = abs(priceDelta)
+    val change = NumberFormat.getCurrencyInstance().also { it.currency = currency }.format(absDelta)
+    val percentChange = (absDelta / price * 100).round(2)
+    return "$sign$change ($percentChange%)"
+}
+
+fun priceDeltaColor(priceDelta: Double) = when {
+    priceDelta > 0 -> R.color.stock_raise
+    priceDelta < 0 -> R.color.stock_fall
+    else -> R.color.black
 }
