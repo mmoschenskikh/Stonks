51786d02e1d63f3f2d3b1ccb1fac93f256991324
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-03-25 23:32:06 +0300

    Support for both list types (all stocks and favourite only)

--- app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockListFragment.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/stocklist/StockListFragment.kt
@@ -1,5 +1,10 @@
 package ru.maxultra.stonks.ui.stocklist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
 import ru.maxultra.stonks.databinding.FragmentStockListBinding
 import ru.maxultra.stonks.ui.base.BaseFragment
@@ -9,8 +14,28 @@ class StockListFragment :
 
     private val viewModelFactory by lazy { StockListViewModelFactory(requireContext()) }
     private val viewModel by activityViewModels<StockListViewModel> { viewModelFactory }
+    private lateinit var listType: StockListType
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = super.onCreateView(inflater, container, savedInstanceState)
+        arguments?.let { listType = it.get(ARG_LIST_TYPE) as StockListType }
+        return root
+    }
 
     companion object {
-        fun newInstance() = StockListFragment()
+        private const val ARG_LIST_TYPE = "StockListFragment_Type"
+
+        fun newInstance(type: StockListType) =
+            StockListFragment().apply {
+                arguments = bundleOf(ARG_LIST_TYPE to type)
+            }
     }
+}
+
+enum class StockListType {
+    LIST, FAVOURITE
 }
--- app/src/main/java/ru/maxultra/stonks/ui/tabs/TabsAdapter.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/tabs/TabsAdapter.kt
@@ -3,11 +3,19 @@ package ru.maxultra.stonks.ui.tabs
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import ru.maxultra.stonks.ui.stocklist.StockListFragment
+import ru.maxultra.stonks.ui.stocklist.StockListType
 
 class TabsAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount() = PAGE_COUNT
 
-    override fun createFragment(position: Int) = StockListFragment.newInstance()
+    override fun createFragment(position: Int): Fragment {
+        val type = when (position) {
+            0 -> StockListType.LIST
+            1 -> StockListType.FAVOURITE
+            else -> throw IllegalArgumentException("No tab expected at position #$position")
+        }
+        return StockListFragment.newInstance(type)
+    }
 
     companion object {
         private const val PAGE_COUNT = 2
