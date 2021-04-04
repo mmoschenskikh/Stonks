2f62e579de55ac3f2e82d573c7ae5fb4ebecfd1e
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-04-04 21:30:59 +0300

    Stubs for stock details

---
+++ app/src/main/java/ru/maxultra/stonks/ui/details/ChartFragment.kt
@@ -0,0 +1,6 @@
+package ru.maxultra.stonks.ui.details
+
+import ru.maxultra.stonks.databinding.FragmentChartBinding
+import ru.maxultra.stonks.ui.base.BaseFragment
+
+class ChartFragment : BaseFragment<FragmentChartBinding>(FragmentChartBinding::inflate)
---
+++ app/src/main/java/ru/maxultra/stonks/ui/details/SummaryFragment.kt
@@ -0,0 +1,6 @@
+package ru.maxultra.stonks.ui.details
+
+import ru.maxultra.stonks.databinding.FragmentSummaryBinding
+import ru.maxultra.stonks.ui.base.BaseFragment
+
+class SummaryFragment : BaseFragment<FragmentSummaryBinding>(FragmentSummaryBinding::inflate)
