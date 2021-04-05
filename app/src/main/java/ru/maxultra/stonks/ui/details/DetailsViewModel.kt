df3f491ac7af4bab47f0c3ec56d1050b6e017345
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-04-05 21:15:25 +0300

    ViewModel preparing chart and handling errors

--- app/src/main/java/ru/maxultra/stonks/ui/details/DetailsViewModel.kt
+++ app/src/main/java/ru/maxultra/stonks/ui/details/DetailsViewModel.kt
@@ -1,12 +1,15 @@
 package ru.maxultra.stonks.ui.details
 
 import android.content.Context
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import com.github.mikephil.charting.data.Entry
+import kotlinx.coroutines.launch
+import ru.maxultra.stonks.data.adapter
 import ru.maxultra.stonks.data.database.StockDatabase
 import ru.maxultra.stonks.data.network.StonksNetwork
 import ru.maxultra.stonks.data.repository.StockRepository
 import ru.maxultra.stonks.ui.base.BaseViewModel
+import ru.maxultra.stonks.util.Status
 
 class DetailsViewModel(
     private val stockRepository: StockRepository
@@ -15,8 +18,33 @@ class DetailsViewModel(
     var stock = stockRepository.getStock("")
         private set
 
+    private val _status = MutableLiveData(Status.LOADING)
+    val status: LiveData<Status>
+        get() = _status
+
     fun setTicker(ticker: String) {
         stock = stockRepository.getStock(ticker)
+        refreshStock(ticker)
+    }
+
+    fun refreshStock(ticker: String) {
+        viewModelScope.launch {
+            try {
+                _status.value = Status.LOADING
+                stockRepository.fetchStock(ticker)
+                stockRepository.fetchThirtyMinChart(ticker)
+                _status.value = Status.SUCCESS
+            } catch (e: Exception) {
+                _status.value = Status.ERROR
+            }
+        }
+    }
+
+    fun getDisplayChart(dbChart: String?): List<Entry> {
+        if (dbChart.isNullOrBlank()) return emptyList()
+        return adapter.fromJson(dbChart)
+            ?.map { Entry(it.date.toFloat(), it.price) }
+            ?.reversed() ?: emptyList()
     }
 }
 
